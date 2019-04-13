dc79bbb09dbc956c93be3d379f48c9e42c9159d7
everyx <lunt.luo@gmail.com>
2019-04-13 16:23:44 +0800

    Updated apps

--- bin/update.ps1
+++ bin/update.ps1
@@ -1,3 +1,5 @@
+git pull
+
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
 $checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
 $formatjson = "$env:SCOOP_HOME/bin/formatjson.ps1"
@@ -7,7 +9,6 @@ $bucket = "$psscriptroot/../bucket" # checks the parent dir
 Invoke-Expression -Command "$checkver -dir $bucket * -u"
 Invoke-Expression -Command "$formatjson -path $bucket"
 
-git pull
 git add .
 git commit -m "Updated apps"
 git push
