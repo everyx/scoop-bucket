8a1fc578215be1e993f773cc5b9f17448a3b5c63
everyx <lunt.luo@gmail.com>
2019-05-11 10:33:52 +0800

    Updated apps

--- bin/update.ps1
+++ bin/update.ps1
@@ -7,7 +7,7 @@ $formatjson = "$env:SCOOP_HOME/bin/formatjson.ps1"
 $bucket = "$psscriptroot/../bucket" # checks the parent dir
 
 Invoke-Expression -Command "$checkver -dir $bucket * -u"
-Invoke-Expression -Command "$formatjson -path $bucket"
+Invoke-Expression -Command "$formatjson -dir $bucket"
 
 git add .
 git commit -m "Updated apps"
