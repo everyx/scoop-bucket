fd06a537b676abc6790e44d613a76598d8e097be
everyx <lunt.luo@gmail.com>
2019-09-29 15:41:42 +0800

    Updated apps

--- bin/update.ps1
+++ bin/update.ps1
@@ -1,5 +1,6 @@
 git pull
 
+# check update
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
 $checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
 $formatjson = "$env:SCOOP_HOME/bin/formatjson.ps1"
@@ -9,6 +10,22 @@ $bucket = "$psscriptroot/../bucket" # checks the parent dir
 Invoke-Expression -Command "$checkver -dir $bucket * -u"
 Invoke-Expression -Command "$formatjson -dir $bucket"
 
+# generate fonts
+$rootdir = "$psscriptroot/.."
+
+$version = (Get-Content -Raw -Path "$rootdir/bucket/CascadiaCode-NF.json" | ConvertFrom-Json).version
+Invoke-WebRequest `
+    -Uri "https://github.com/microsoft/cascadia-code/releases/download/v$version/Cascadia.ttf" `
+    -OutFile "$rootdir/fonts/Cascadia.ttf"
+
+docker-compose -f "$psscriptroot/docker-compose.yml" up --build
+
+del "$rootdir/fonts/Cascadia.ttf"
+mv "$rootdir/fonts/Cascadia Code Nerd Font Windows Compatible.ttf" "$rootdir/fonts/Cascadia-NF.ttf"
+
+docker-compose -f "$psscriptroot/docker-compose.yml" down
+
+# push
 git add .
 git commit -m "Updated apps"
 git push
