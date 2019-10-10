473f14478f57fdebc549788be9dceda59b89a04f
everyx <lunt.luo@gmail.com>
2019-10-10 16:19:18 +0800

    Updated apps

--- bin/update.ps1
+++ bin/update.ps1
@@ -13,22 +13,28 @@ Invoke-Expression -Command "$checkver -Dir $bucketDir -Update"
 $manifest = "CascadiaCode-NF"
 $fontsDir = "$psscriptroot/../fonts"
 $version = (Get-Content -Raw -Path "$bucketDir/$manifest.json" | ConvertFrom-Json).version
-Invoke-WebRequest `
-    -Uri "https://github.com/microsoft/cascadia-code/releases/download/v$version/Cascadia.ttf" `
-    -OutFile "$fontsDir/Cascadia.ttf"
-
 $fontFile = "$fontsDir/Cascadia-NF_$version.ttf"
 
 if (![System.IO.File]::Exists($fontFile)) {
+    Invoke-WebRequest `
+        -Uri "https://github.com/microsoft/cascadia-code/releases/download/v$version/Cascadia.ttf" `
+        -OutFile "$fontsDir/Cascadia.ttf"
+
     docker-compose -f "$psscriptroot/docker-compose.yml" up --build
+
+    Remove-Item "$fontsDir/Cascadia.ttf"
+
     Move-Item -Force `
         -Path "$fontsDir/Cascadia Code Nerd Font Windows Compatible.ttf" `
         -Destination "$fontFile"
+
+    Remove-Item "$fontsDir/Cascadia-NF_*.ttf" -Exclude "Cascadia-NF_$version.ttf"
+
     docker-compose -f "$psscriptroot/docker-compose.yml" down
 
     # push fonts
     git add "$fontsDir"
-    git commit -m "update CascadiaCode-NF to v$version."
+    git commit -m "update CascadiaCode-NF to v$version"
     git push
 
     $checkhashes = "$env:SCOOP_HOME/bin/checkhashes.ps1"
