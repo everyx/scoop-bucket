230e2f8c0a0766c6e3147d08973f53d8feb054f2
everyx <lunt.luo@gmail.com>
2020-09-15 14:11:18 +0800

    correct yacd version

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -5,4 +5,4 @@ $checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
 
 $dir = "$psscriptroot/../bucket" # checks the parent dir
 
-Invoke-Expression -command "& '$checkver' -dir '$dir' $($args | ForEach-Object { "$_ " })"
+Invoke-Expression -command "& '$checkver' -Dir '$dir' $($args | ForEach-Object { "$_ " })"
